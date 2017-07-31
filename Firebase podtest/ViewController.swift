85042a32df4d07f71d7d548bb18b6d8bdd127a68
bobby zhang <zhang.bobby98@gmail.com>
2017-07-30 20:47:02 -0400

    Initial Commit

---
+++ Firebase podtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase podtest
+//
+//  Created by Bobby Zhang on 2017-07-30.
+//  Copyright © 2017 bobby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
